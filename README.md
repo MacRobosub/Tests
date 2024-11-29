6a2cbc378f7b4a8409f3c242fbaf0fb7af791c91
Mohamed Sabri <sabrim2@mcmaster.ca>
2024-11-29 18:03:08 -0500

    New Locations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tests
-Testing various designs and functionalities
+Testing various designs and functionalities for the website
+Not actual website
+Use Yazan's folder for templates if needed
--- script2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Show the navigation bar after the fade animations are complete
-    setTimeout(() => {
-        const navbar = document.getElementById("navbar");
-        navbar.style.display = "block";
-        navbar.style.animation = "fadeIn 2s ease-out";
-    }, 7000); // 7-second delay to match the fade animation
-});
